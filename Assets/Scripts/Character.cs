d0856f62e887d192109d294cd92bcf0f666bc787
MyName <email@gmail.com>
2020-10-14 18:48:34 +0300

    add solution for task 1-1

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -13,27 +13,38 @@ public class Character : MonoBehaviour
         Attack,
         BeginShoot,
         Shoot,
+        BeginFistAttack,
+        FistAttack,
+        Dead
     }
 
     public enum Weapon
     {
         Pistol,
         Bat,
+        Fist
     }
 
     Animator animator;
     State state;
 
     public Weapon weapon;
-    public Transform target;
+    public GameObject target;
+    Transform targetTransform;
+    Character targetCharacter;
+
     public float runSpeed;
     public float distanceFromEnemy;
+
     Vector3 originalPosition;
     Quaternion originalRotation;
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        targetTransform = target.GetComponent<Transform>();
+        targetCharacter = target.GetComponent<Character>();
+
         state = State.Idle;
         originalPosition = transform.position;
         originalRotation = transform.rotation;
@@ -44,15 +55,27 @@ public class Character : MonoBehaviour
         state = newState;
     }
 
+    public State GetState()
+    {
+        return state;
+    }
+
     [ContextMenu("Attack")]
     void AttackEnemy()
     {
-        switch (weapon) {
+        if (targetCharacter.GetState() == State.Dead) return;
+        if (state == State.Dead) return;
+
+        switch (weapon)
+        {
+            case Weapon.Pistol:
+                state = State.BeginShoot;
+                break;
             case Weapon.Bat:
                 state = State.RunningToEnemy;
                 break;
-            case Weapon.Pistol:
-                state = State.BeginShoot;
+            case Weapon.Fist:
+                state = State.RunningToEnemy;
                 break;
         }
     }
@@ -60,7 +83,8 @@ public class Character : MonoBehaviour
     bool RunTowards(Vector3 targetPosition, float distanceFromTarget)
     {
         Vector3 distance = targetPosition - transform.position;
-        if (distance.magnitude < 0.00001f) {
+        if (distance.magnitude < 0.00001f)
+        {
             transform.position = targetPosition;
             return true;
         }
@@ -72,7 +96,8 @@ public class Character : MonoBehaviour
         distance = (targetPosition - transform.position);
 
         Vector3 step = direction * runSpeed;
-        if (step.magnitude < distance.magnitude) {
+        if (step.magnitude < distance.magnitude)
+        {
             transform.position += step;
             return false;
         }
@@ -83,7 +108,8 @@ public class Character : MonoBehaviour
 
     void FixedUpdate()
     {
-        switch (state) {
+        switch (state)
+        {
             case State.Idle:
                 transform.rotation = originalRotation;
                 animator.SetFloat("Speed", 0.0f);
@@ -91,7 +117,7 @@ public class Character : MonoBehaviour
 
             case State.RunningToEnemy:
                 animator.SetFloat("Speed", runSpeed);
-                if (RunTowards(target.position, distanceFromEnemy))
+                if (RunTowards(targetTransform.position, distanceFromEnemy))
                     state = State.BeginAttack;
                 break;
 
@@ -107,6 +133,7 @@ public class Character : MonoBehaviour
                 break;
 
             case State.Attack:
+                targetCharacter.SetState(State.Dead);
                 break;
 
             case State.BeginShoot:
@@ -115,6 +142,20 @@ public class Character : MonoBehaviour
                 break;
 
             case State.Shoot:
+                targetCharacter.SetState(State.Dead);
+                break;
+
+            case State.BeginFistAttack:
+                animator.SetTrigger("FistAttack");
+                state = State.FistAttack;
+                break;
+
+            case State.FistAttack:
+                targetCharacter.SetState(State.Dead);
+                break;
+
+            case State.Dead:
+                animator.SetTrigger("Death");
                 break;
         }
     }
